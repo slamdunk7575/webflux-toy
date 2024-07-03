74d49f2e63bf8d53761032a86c354d93bebe2d4e
slamdunk7575 <slamdunk7575@gmail.com>
2024-07-03 19:39:32 +0900

    (feat) User 도메인 추가

---
+++ src/main/java/com/yanggang/webflux/repository/User.java
@@ -0,0 +1,19 @@
+package com.yanggang.webflux.repository;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class User {
+
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
