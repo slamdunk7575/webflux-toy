32efe17cb9eb6333ba4de75f4aea0587095a1d57
slamdunk7575 <slamdunk7575@gmail.com>
2024-07-26 01:05:02 +0900

    (feat) R2DBC 디펜던시 및 설정 추가

---
+++ src/main/java/com/yanggang/webflux/config/R2dbcConfig.java
@@ -0,0 +1,35 @@
+package com.yanggang.webflux.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.r2dbc.core.DatabaseClient;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@EnableR2dbcAuditing
+@EnableR2dbcRepositories
+public class R2dbcConfig implements ApplicationListener<ApplicationReadyEvent> {
+
+    private final DatabaseClient databaseClient;
+
+    public R2dbcConfig(DatabaseClient databaseClient) {
+        this.databaseClient = databaseClient;
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        databaseClient.sql("SELECT 1").fetch().one()
+                .subscribe(
+                        success -> {
+                            log.info("Initialize R2DBC database connection");
+                        },
+                        error -> {
+                            log.info("Failed to initialize R2DBC database connection");
+                        }
+                );
+    }
+}
